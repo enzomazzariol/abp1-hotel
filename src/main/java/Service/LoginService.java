23b2c20da8d186a7d585a8a0c3cc03acec4853aa
santichenoll3 <santichenoll3@gmail.com>
2024-10-01 11:37:23 +0200

    Lógica de comprobar el login en LoginService

--- src/main/java/Service/LoginService.java
+++ src/main/java/Service/LoginService.java
@@ -1,20 +1,42 @@
 package Service;
 
+import DAO.Conexion;
+import Utils.Constants;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginService {
 
     // POST
-    public void comprobarLogin(HttpServletRequest req, HttpServletResponse resp) {
+    public void comprobarLogin(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ClassNotFoundException {
         String nombre = req.getParameter("nombre");
         String password = req.getParameter("password");
 
         // Logica para comprobar el nombre y la contraseña.
+        Connection conexion = new Conexion().conectar();
+        try (PreparedStatement ps = conexion.prepareStatement(Constants.SELECT_USUARIOS_NOMBRE_CONTRASENA)) {
+            ps.setString(1, nombre);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
 
+            if (rs.next()) {
+                req.getSession().setAttribute("nombre", nombre);
+                enviarLogin(req, resp);
+            } else {
+                System.out.println("Error en el usuario o en la contraseña.");
+            }
+
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("El usuario es " + nombre + " y la contraseña es " + password);
 
     }
--- src/main/java/Servlets/LoginServlet.java
+++ src/main/java/Servlets/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -20,6 +21,10 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ls.comprobarLogin(req, resp);
+        try {
+            ls.comprobarLogin(req, resp);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
