1426dc5e0b6536f3598af54fa0c4cdf5e74a33f0
luiswrld <luissgon100@gmail.com>
2024-10-12 16:06:05 +0000

    llamada a forward

--- src/main/java/Servlets/UsuariosServlet.java
+++ src/main/java/Servlets/UsuariosServlet.java
@@ -14,7 +14,7 @@ public class UsuariosServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        us.listarUsuarios(req, resp); // Llama al método de listar usuarios
+        us.forwardusuarios(req, resp); // Llama al método de listar usuarios
     }
 
     @Override
